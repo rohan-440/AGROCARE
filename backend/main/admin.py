ef6cf5353b247509110330c9955afdbff735f92d
Rohan Gupta <92628349+rohan-440@users.noreply.github.com>
2024-11-13 08:25:41 +0530

    Update admin.py

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Teacher)
-admin.site.register(models.CourseCategory)
+admin.site.register(models.Farmer)
+admin.site.register(models.Expert)
